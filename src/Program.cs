7c7e69cdca6526d345fea7597b2944d3583b3414
DanilchikB <danil1357997@gmail.com>
2020-04-12 20:09:58 +0300

    Hello World!

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_scheduler_prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
