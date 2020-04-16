5dc586c63524ed2479ad65beeb78b55b9436ac29
DanilchikB <danil1357997@gmail.com>
2020-04-16 13:59:14 +0300

    Created a class for working with the database

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using HelpFunctions.colorText;
 using System.Data.SQLite;
+using DB.WorkWithDB;
 
 namespace console_scheduler_prototype
 {
@@ -8,22 +9,12 @@ namespace console_scheduler_prototype
     {
         static void Main(string[] args)
         {
-            using(SQLiteConnection Connection = new SQLiteConnection("Data Source=./test.db; Version=3;")){
-                string commandText = @"CREATE TABLE IF NOT EXISTS records(
-                        id INTEGER PRIMARY KEY,
-                        record TEXT NOT NULL
-                    )";
-                
-                Connection.Open();
+            
 
-                SQLiteCommand Command = new SQLiteCommand(commandText, Connection);
-                Command.ExecuteNonQuery();
-                string text = "Create variables";
-                Command.CommandText = $@"INSERT INTO records (record)
-                    VALUES('{text}');";
-                Command.ExecuteNonQuery();
-                Connection.Close();
-            }
+           WorkWithDB db = new WorkWithDB();
+            db.ExecuteQueryNoReturn(
+                $@"INSERT INTO records (record)
+                    VALUES('VALUE');");
         }
     }
 }
---
+++ src/WorkWithDB.cs
@@ -0,0 +1,45 @@
+/***
+    Класс для работы с sql запросами для базы данных SQLite
+***/
+
+using System.Data.SQLite;
+
+namespace DB.WorkWithDB{
+    class WorkWithDB{
+        public WorkWithDB(){
+            locationDB = "Data Source=./test.db; Version=3;";
+        }
+        public WorkWithDB(string locationDB){
+            this.locationDB = locationDB;
+        }
+
+        public string locationDB{private get; set;}
+
+        //Делегат для внедрения функции в метод OpenAndCloseDB()
+        public delegate void Query(SQLiteCommand command, string sqlQuery);
+
+        //Запрос без вытаскивания данных
+        public void ExecuteQueryNoReturn(string sqlQuery){
+            Query query = delegate(SQLiteCommand command, string sqlQuery){
+                command.CommandText = sqlQuery;
+                command.ExecuteNonQuery();
+            };
+            OpenAndCloseDB(sqlQuery, locationDB, query);
+        }
+
+        //Метод для открытия и закрытия базы данных
+        private void OpenAndCloseDB(string sqlQuery, string locationDB, Query query){
+            using(SQLiteConnection Connection = new SQLiteConnection(locationDB)){
+                
+                Connection.Open();
+
+                SQLiteCommand Command = new SQLiteCommand(Connection);
+                query.Invoke(Command, sqlQuery);
+
+                Connection.Close();
+            }
+        }
+    } 
+
+}
+
